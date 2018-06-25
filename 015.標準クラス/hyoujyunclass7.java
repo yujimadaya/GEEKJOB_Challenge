a7f53d324c60339ceca81da79025673e17b3307e
山田侑司 <aobee666@gmail.com>
2018-06-25 13:22:39 +0900

    標準クラス　文字の入れ替え　課題提出します。

---
+++ 015.標準クラス/hyoujyunclass7.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp;
+
+/**
+ *
+ * @author yamadayuushi
+ */
+public class hyoujyunclass7 {
+    
+    public static void main(String[] args){
+    
+        String okikae = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます"; //課題の文字列
+            
+        System.out.println(okikae.replace("U","う").replace("I","い")); //Uをうに置換した文字列をさらにIをいに置換した文字列に変更させる
+    
+    }
+}
