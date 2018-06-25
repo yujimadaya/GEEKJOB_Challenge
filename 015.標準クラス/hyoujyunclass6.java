d2a636004114cb194852d7c55e3e15058b72238b
山田侑司 <aobee666@gmail.com>
2018-06-25 13:08:07 +0900

    標準クラス　文字数の所得　課題提出します

---
+++ 015.標準クラス/hyoujyunclass6.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp;
+
+/**
+ *
+ * @author yamadayuushi
+ */
+public class hyoujyunclass6 {
+    
+    public static void main(String[] args){
+    
+    String mail = "aobee888@gmail.com";
+    
+    System.out.println(mail);
+    
+    System.out.println( "@以降の文字数:" + mail.substring(9,18).length());
+    
+    
+    }
+    
+}
