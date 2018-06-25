96ae7ea224cd98160b6f839871311959ff2dbdad
山田侑司 <aobee666@gmail.com>
2018-06-25 12:54:58 +0900

    標準クラス　バイト数と文字数の所得　課題提出します。

---
+++ 015.標準クラス/hyoujyunclass5.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp;
+
+
+/**
+ *
+ * @author yamadayuushi
+ */
+public class hyoujyunclass5 {
+    
+    public static void main(String[] args){
+    
+        String myName = "山田侑司";
+        
+        System.out.println(myName);
+        
+        System.out.println("文字数:" + myName.length()); //文字数
+        
+        System.out.println("バイト数:" + myName.getBytes().length); //バイト数
+    
+        
+        
+        
+        
+    }
+    
+}
